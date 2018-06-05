df8abec9648e7472fdce196908202180ce07b32b
vishwakarmasunil68 <vishwakarmasunil68@gmail.com>
2018-06-06 01:45:20 +0530

    Initial commit

---
+++ app/src/main/java/com/sundroid/tagutil/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sundroid.tagutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sundroid.tagutillib.TagUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
